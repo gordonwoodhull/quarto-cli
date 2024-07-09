e45ecefc71cf9453c53a79f07bf121e4871ffe48
Gordon Woodhull <gordon.woodhull@posit.co>
2024-07-09 18:00:51 -0400

    demo brand.yml in typst css

--- src/resources/filters/quarto-post/typst-css-to-props.lua
+++ src/resources/filters/quarto-post/typst-css-to-props.lua
@@ -911,8 +911,19 @@ function render_typst_css_to_props()
     return span
   end
 
-  quarto.log.output('brand', params('brand'))
   return {
+    Pandoc = function(pandoc)
+      local brand = param('brand')
+      local lines = {}
+      if brand.color and brand.color.palette then
+        table.insert(lines, '#let brand-palette = (')
+        for name, color in pairs(brand.color.palette) do
+          table.insert(lines, '  ' .. name .. ': ' .. output_typst_color(parse_css_color(color)) .. ',')
+        end
+        table.insert(lines, ')')
+        quarto.doc.include_text('before-body', table.concat(lines, '\n'))
+      end
+    end,
     Table = function(tab)
       local tabstyle = tab.attributes['style']
       if tabstyle ~= nil then
