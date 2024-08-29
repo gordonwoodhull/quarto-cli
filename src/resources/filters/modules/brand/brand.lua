c5172aa645ac838b64b76aaa7735de57574853d0
Gordon Woodhull <gordon.woodhull@posit.co>
2024-08-29 09:33:09 -0400

    typst brand yaml: logo

--- src/resources/filters/modules/brand/brand.lua
+++ src/resources/filters/modules/brand/brand.lua
@@ -2,7 +2,9 @@
 -- Copyright (C) 2020-2024 Posit Software, PBC
 
 local function get_color(name)
-  local brand = param("brand").processedData -- from src/core/brand/brand.ts
+  local brand = param("brand")
+  brand = brand and brand.processedData -- from src/core/brand/brand.ts
+  if not brand then return nil end
   local cssColor = brand.color[name]
   if not cssColor then return nil end
   if _quarto.format.isTypstOutput() then
@@ -11,21 +13,38 @@ local function get_color(name)
   return cssColor
 end
 
-local BACKGROUND_OPACITY = 0.1
-
-local function get_background_color(name)
-  local brand = param("brand").processedData -- from src/core/brand/brand.ts
+local function get_background_color(name, opacity)
+  local brand = param("brand")
+  brand = brand and brand.processedData -- from src/core/brand/brand.ts
+  if not brand then return nil end
   local cssColor = brand.color[name]
   if not cssColor then return nil end
   if _quarto.format.isTypstOutput() then
-    return output_typst_color(parse_css_color(cssColor), {unit = 'fraction', value = BACKGROUND_OPACITY})
+    return output_typst_color(parse_css_color(cssColor), {unit = 'fraction', value = opacity})
   end
   -- todo: implement for html if useful
   return cssColor
 end
 
+local function get_typography(fontName)
+  local brand = param("brand")
+  brand = brand and brand.processedData -- from src/core/brand/brand.ts
+  if not brand then return nil end
+  -- todo: convert typography options from CSS to Typst
+  return brand.typography and brand.typography[fontName]
+end
+
+local function get_logo(name)
+  local brand = param("brand")
+  brand = brand and brand.processedData -- from src/core/brand/brand.ts
+  if not brand then return nil end
+  -- todo convert logo options from CSS to Typst
+  return brand.logo and brand.logo[name]
+end
+
 return {
-  BACKGROUND_OPACITY = BACKGROUND_OPACITY,
   get_color = get_color,
   get_background_color = get_background_color,
+  get_typography = get_typography,
+  get_logo = get_logo,
 }
--- src/resources/filters/quarto-post/typst-brand-yaml.lua
+++ src/resources/filters/quarto-post/typst-brand-yaml.lua
@@ -34,10 +34,30 @@ function render_typst_brand_yaml()
     return '(\n' .. inside .. table.concat(entries, ',\n' .. inside) .. '\n' .. curr .. ')'
   end
 
+  local horz_to_typst = {
+    left = "left",
+    center = "center",
+    right = "right",
+  }
+  local vert_to_typst = {
+    top = "top",
+    middle = "horizon",
+    bottom = "bottom",
+  }
+  
+  local function location_to_typst_align(location)
+    local _, ndash = location:gsub('-', '')
+    if ndash ~= 1 then return nil end
+    local horz, vert = location:match '(%a+)--(%a+)'
+    quarto.log.output('lota', horz, vert)
+    if not horz_to_typst[horz] or not vert_to_typst[vert] then return nil end
+    quarto.log.output('lota3', horz, vert)
+    return horz_to_typst[horz] .. '+' .. vert_to_typst[vert]
+  end  
+
   return {
     Pandoc = function(pandoc)
       local brand = param('brand')
-
       if brand and brand.processedData then
         -- color
         if brand.processedData.color and next(brand.processedData.color) then
@@ -51,7 +71,7 @@ function render_typst_brand_yaml()
           local BACKGROUND_OPACITY = 0.1
           local themebk = {}
           for name, _ in pairs(brandColor) do
-            themebk[name] = _quarto.modules.brand.get_background_color(name)
+            themebk[name] = _quarto.modules.brand.get_background_color(name, BACKGROUND_OPACITY)
           end
           -- for demo purposes only, should implement backgroundcolor and fontcolor
           if brandColor.background then
@@ -60,37 +80,80 @@ function render_typst_brand_yaml()
           if brandColor.foreground then
             quarto.doc.include_text('in-header', '#set text(fill: brand-color.foreground)')
           end
-          local decl = '// theme colors at opacity ' .. _quarto.modules.brand.BACKGROUND_OPACITY .. '\n#let brand-color-background = ' .. to_typst_dict_indent(themebk)
+          local decl = '// theme colors at opacity ' .. BACKGROUND_OPACITY .. '\n#let brand-color-background = ' .. to_typst_dict_indent(themebk)
           quarto.doc.include_text('in-header', decl)
         end
 
         -- typography
-        if brand.processedData.typography then
-          local typography = brand.processedData.typography
-          if typography.headings and typography.headings.family then
-            quarto.doc.include_text('in-header', '#show heading: set text(font: "' .. typography.headings.family .. '")')
-          elseif typography.monospace and typography.monospace.family then
-            quarto.doc.include_text('in-header', '#show raw: set text(font: "' .. typography.monospace.family .. '")')
+        local headings = _quarto.modules.brand.get_typography('headings')
+        if headings and headings.family then
+          quarto.doc.include_text('in-header', '#show heading: set text(font: "' .. headings.family .. '")')
+        end
+        local monospace = _quarto.modules.brand.get_typography('monospace')
+        if monospace and monospace.family then
+            quarto.doc.include_text('in-header', '#show raw: set text(font: "' .. monospace.family .. '")')
+        end
+
+        -- logo
+        local logo = param('logo')
+        local logoOptions = {}
+        local foundSrc = null
+         if logo then
+          if type(logo) == 'string' then
+            foundSrc = _quarto.modules.brand.get_logo(logo) or logo
+          elseif type(logo) == 'table' then
+            for k, v in pairs(logo) do
+              logoOptions[k] = v
+            end
+            if logo.src then
+              foundSrc =  _quarto.modules.brand.get_logo(logo.src) or logo.src
+            end
           end
         end
+        if not foundSrc and brand.processedData.logo then
+          local tries = {'large', 'small', 'medium'} -- low to high priority
+          for _, try in ipairs(tries) do
+            local src = _quarto.modules.brand.get_logo(try)
+            if src then
+              foundSrc = src
+            end
+          end
+        end
+        if foundSrc then
+          if type(foundSrc) == "string" then
+            logoOptions.src = foundSrc
+          elseif foundSrc.light then
+            logoOptions.src = foundSrc.light
+          elseif foundSrc.dark then
+            logoOptions.src = foundSrc.dark
+          end
+          -- todo: resolve logoOptions.src path
+          logoOptions.padding = logoOptions.padding or '0.5in'
+          logoOptions.width = logoOptions.width or '2in'
+          logoOptions.location = logoOptions.location and
+            location_to_typst_align(logoOptions.location) or 'left+top'
+          quarto.log.debug('logo options', logoOptions)
+          quarto.doc.include_text('in-header',
+            '#set page(background: align(' .. logoOptions.location .. ', box(inset: ' .. logoOptions.padding .. ', image("' .. logoOptions.src .. '", width: ' .. logoOptions.width .. '))))')
+        end  
       end
     end,
     Meta = function(meta)
-      local brand = param('brand')
-      if not brand or not brand.processedData or not brand.processedData.typography then return nil end
-      local typography = brand.processedData.typography
-      if typography.base and typography.base.family then
-        meta['mainfont'] = typography.base.family
+      local base = _quarto.modules.brand.get_typography('base')
+      if base and base.family then
+        meta['mainfont'] = base.family
       end
-      if typography.headings and typography.headings.family then
-        meta['title-font'] = typography.headings.family
+      local headings = _quarto.modules.brand.get_typography('headings')
+      if headings and headings.family then
+        meta['title-font'] = headings.family
       end
       -- really should be _brand.yml directory
       local projectDir = pandoc.path.directory(quarto.doc.input_file)
       local fontdirs = {}
-      for _, sel in ipairs({'base', 'headings', 'monospace'}) do
-        if typography[sel] and typography[sel].files then
-          local files = typography[sel].files
+      for _, fontName in ipairs({'base', 'headings', 'monospace'}) do
+        local font = _quarto.modules.brand.get_typography(fontName)
+        if font and font.files then
+          local files = font.files
           if type(files) == 'string' then files = {files} end
           for _, file in ipairs(files) do
             local dir = pandoc.path.directory(pandoc.path.normalize(pandoc.path.join({projectDir, file})))
