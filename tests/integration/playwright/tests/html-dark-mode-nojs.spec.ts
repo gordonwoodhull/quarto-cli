6e1855c2926ec534020d54822af9ed817a29e466
Gordon Woodhull <gordon.woodhull@posit.co>
2025-04-14 14:05:29 -0400

    add tests for a11y and arrow

--- tests/integration/playwright/tests/html-dark-mode-nojs.spec.ts
+++ tests/integration/playwright/tests/html-dark-mode-nojs.spec.ts
@@ -22,7 +22,7 @@ test('Light brand default, no JS', async ({ page }) => {
 });
 
 
-test('Syntax highlighting, no JS', async ({ page }) => {
+test('Syntax highlighting, a11y, no JS', async ({ page }) => {
   // This document use a custom theme file that change the background color of the title banner
   // Same user defined color should be used in both dark and light theme
   await page.goto('./html/dark-brand/syntax-highlighting/syntax-highlighting.html');
@@ -33,6 +33,17 @@ test('Syntax highlighting, no JS', async ({ page }) => {
   await expect(importKeyword).toHaveCSS('color', 'rgb(84, 84, 84)');
 });
 
+
+test('Syntax highlighting, arrow, no JS', async ({ page }) => {
+  // This document use a custom theme file that change the background color of the title banner
+  // Same user defined color should be used in both dark and light theme
+  await page.goto('./html/dark-brand/syntax-highlighting/arrow-syntax-highlighting.html');
+  const locatr = await page.locator('body').first();
+  await expect(locatr).toHaveClass('fullcontent quarto-light');
+  await expect(locatr).toHaveCSS('background-color', 'rgb(255, 255, 255)');
+  const link = await page.locator('span.al').first();
+  await expect(link).toHaveCSS('background-color', 'rgba(0, 0, 0, 0)');
+});
 
 test('Project dark brand default, no JS', async ({ page }) => {
   // This document use a custom theme file that change the background color of the title banner
--- tests/integration/playwright/tests/html-dark-mode.spec.ts
+++ tests/integration/playwright/tests/html-dark-mode.spec.ts
@@ -33,3 +33,27 @@ test('Brand false remove project brand', async ({ page }) => {
   // no toggle
   expect(await page.locator('a.quarto-color-scheme-toggle').count()).toEqual(0);
 });
+
+
+test('Syntax highlighting, a11y, with JS', async ({ page }) => {
+  // This document use a custom theme file that change the background color of the title banner
+  // Same user defined color should be used in both dark and light theme
+  await page.goto('./html/dark-brand/syntax-highlighting/syntax-highlighting.html');
+  const locatr = await page.locator('body').first();
+  await expect(locatr).toHaveClass('fullcontent quarto-light');
+  await expect(locatr).toHaveCSS('background-color', 'rgb(255, 255, 255)');
+  const importKeyword = await page.locator('span.im').first();
+  await expect(importKeyword).toHaveCSS('color', 'rgb(84, 84, 84)');
+});
+
+
+test('Syntax highlighting, arrow, with JS', async ({ page }) => {
+  // This document use a custom theme file that change the background color of the title banner
+  // Same user defined color should be used in both dark and light theme
+  await page.goto('./html/dark-brand/syntax-highlighting/arrow-syntax-highlighting.html');
+  const locatr = await page.locator('body').first();
+  await expect(locatr).toHaveClass('fullcontent quarto-light');
+  await expect(locatr).toHaveCSS('background-color', 'rgb(255, 255, 255)');
+  const link = await page.locator('span.al').first();
+  await expect(link).toHaveCSS('background-color', 'rgba(0, 0, 0, 0)');
+});
